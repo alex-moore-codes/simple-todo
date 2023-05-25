3957fd6a1c3701df8852c168916f60da9759c07c
alex-moore-codes <alex.moore.codes@gmail.com>
2023-05-25 09:08:20 -0400

    built layout.js with fonts and h1 for app title

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,30 @@
 import './globals.css';
-import { Montserrat } from 'next/font/google';
+import { ReactNode } from 'react';
+import { Raleway, Montserrat } from 'next/font/google';
 
 const mont = Montserrat({ subsets: ['latin'] });
+const raleway = Raleway({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Simple Todo App',
   description: 'Manage your todos with this simple app.',
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+type RootLayoutProps = {
+  children: ReactNode;
+};
+
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
-      <body className={mont.className}>{children}</body>
+      <body className={`${raleway.className} pt-[32px]`}>
+        <h1 className="text-center text-[36px] leading-[42.26px] font-bold tracking-tighter">
+          &#x23;todo
+        </h1>
+        <main className={`${mont.className} w-2/5 flex flex-col items-center`}>
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
