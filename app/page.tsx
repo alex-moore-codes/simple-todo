97419dcac210d1ba129d38f8ec9fc98ad7f6906f
alex-moore-codes <alex.moore.codes@gmail.com>
2023-05-25 09:59:41 -0400

    AddTask.jsx UI complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import AddTask from '@/components/AddTask';
 import Menu from '@/components/Menu';
 
 export default function Home() {
   return (
     <section className="mt-[40px] w-2/5">
       <Menu />
+      <AddTask />
     </section>
   );
 }
--- components/AddTask.tsx
+++ components/AddTask.tsx
@@ -1,5 +1,13 @@
-import React from 'react';
-
 export default function AddTask() {
-  return <div>AddTask</div>;
+  return (
+    <div className="flex justify-between space-x-[25px] mt-[18px]">
+      <input
+        placeholder="add details"
+        className="rounded-[12px] py-[20px] px-[12px] border-solid border-[1px] border-[#BDBDBD] w-full"
+      />
+      <button className="rounded-[12px] bg-primary text-[14px] font-semibold leading-[17.07px] w-[109px] text-[#FFFFFF]">
+        Add
+      </button>
+    </div>
+  );
 }
